7b63696d46955febadcb570f1da5890d46d9cb0d
kiwoonjung <kiwoon0627@gmail.com>
2025-05-30 02:27:45 -0700

    update knexfile.cjs

--- knexfile.cjs
+++ knexfile.cjs
@@ -37,18 +37,21 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
+    client: "pg",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      directory: "./migrations",
     },
+    useNullAsDefault: true,
   },
 };
